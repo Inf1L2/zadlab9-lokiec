a202060d829b314427a8a7c8717f623efa8cb09d
lokyiec <pawel@keryio.pl>
2018-05-16 03:30:44 +0200

    lab9

--- OOPL9/src/main/java/pl/edu/ur/oopl9/Main.java
+++ OOPL9/src/main/java/pl/edu/ur/oopl9/Main.java
@@ -3,19 +3,35 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pl.edu.ur.oopl9;
+
+package src.main.java.pl.edu.ur.oopl9;
+import java.util.*;
 
 /**
  *
- * @author maarchyl
+ * @author lokyiec
  */
-public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+public class Main {
     public static void main(String[] args) {
-        // TODO code application logic here
+        int w;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Wybierz zadanie: [1, 2, 4]");
+        w = s.nextInt();
+
+        switch (w) {
+            case 1:
+                Zad_1.Zad_1();
+                break;
+            case 2:
+                Zad_2.Zad_2();
+                break;
+            case 4:
+                Zad_4.Zad_4();
+                break;
+            default:
+                System.out.println("ERROR 404");
+                break;
+        }
     }
-    
 }
